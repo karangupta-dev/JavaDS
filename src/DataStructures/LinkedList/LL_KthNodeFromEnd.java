f3429d5f52bd2f4f41a6a1190194242b623db1c2
Karan Gupta <karangupta.dev@gmail.com>
2025-07-03 14:39:04 -0500

    Kth Node Problem

---
+++ src/DataStructures/LinkedList/LL_KthNodeFromEnd.java
@@ -0,0 +1,48 @@
+package DataStructures.LinkedList;
+
+public class LL_KthNodeFromEnd extends LinkedList {
+
+    /*
+    *   LL_KthNodeFromEnd myLinkedList = new LL_KthNodeFromEnd(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+        myLinkedList.append(5);
+
+        int k = 2;
+        int result = myLinkedList.findKthFromEnd(k).getValue();
+
+        System.out.println(result); // Output: 4
+    *
+    *
+    * */
+
+
+    public LL_KthNodeFromEnd(final int value) {
+        super(value);
+    }
+
+    public Node findKthFromEnd(final int index) {
+
+        Node slow = getHead();
+        Node fast = getHead();
+
+        for (int i = 0; i < index; i++) {
+            if (fast == null) {
+                return null;
+            }
+
+            fast = fast.next;
+        }
+
+        while (fast != null) {
+
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
-import DataStructures.LinkedList.LL_HasLoop;
+import DataStructures.LinkedList.LL_KthNodeFromEnd;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        LL_HasLoop myLinkedList = new LL_HasLoop(1);
+        LL_KthNodeFromEnd myLinkedList = new LL_KthNodeFromEnd(1);
         myLinkedList.append(2);
         myLinkedList.append(3);
         myLinkedList.append(4);
         myLinkedList.append(5);
 
-        // create a loop by connecting the tail to the second node
-//        myLinkedList.getTail().setNext(myLinkedList.getHead().getNext()) ;
+        int k = 2;
+        int result = myLinkedList.findKthFromEnd(k).getValue();
 
-        System.out.println("Has Loop:");
-        System.out.print( myLinkedList.hasLoop());
+        System.out.println(result); // Output: 4
+
     }
 }
