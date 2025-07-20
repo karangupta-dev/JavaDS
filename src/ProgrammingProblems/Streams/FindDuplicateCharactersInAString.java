b09401aeedf70de71f6148ce4f24beced011dc40
Karan Gupta <karangupta.dev@gmail.com>
2025-07-20 17:10:30 -0500

    Add method to count duplicate characters in string
    Introduced countCharacters method to count and print occurrences of each character in a string, sorted by frequency. Updated main method to demonstrate functionality with the input 'programming'.

--- src/ProgrammingProblems/Streams/FindDuplicateCharactersInAString.java
+++ src/ProgrammingProblems/Streams/FindDuplicateCharactersInAString.java
@@ -1,8 +1,30 @@
 package ProgrammingProblems.Streams;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
 public class FindDuplicateCharactersInAString {
 
+    private static void countCharacters(String str) {
+
+        Map<String, Integer> map = new LinkedHashMap<>();
+
+        Arrays.stream(str.split("")).forEach(c -> {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        });
+
+        map.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .forEach(entry -> {
+            System.out.println(String.format("%s occurred %d times", entry.getKey(), entry.getValue()));
+        });
+    }
+
     public static void main(String[] args) {
-        
+        countCharacters("programming");
+
     }
 }
