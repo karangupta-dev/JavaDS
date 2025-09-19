b823776b2ca5c305f8bee047fa27e4f5c8f97c06
Karan Gupta <karangupta.dev@gmail.com>
2025-09-19 17:42:36 -0500

    Add SpiralMatrix solution for spiral order traversal
    Implements the SpiralMatrix class with a method to return elements of a 2D matrix in spiral order. Includes a main method with sample test cases for demonstration.

---
+++ src/DataStructures/Arrays/Medium/SpiralMatrix.java
@@ -0,0 +1,78 @@
+package DataStructures.Arrays.Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> res = new ArrayList<Integer>();
+
+        if (matrix.length == 0) {
+            return res;
+        }
+
+        int rowBegin = 0;
+        int rowEnd = matrix.length - 1;
+        int colBegin = 0;
+        int colEnd = matrix[0].length - 1;
+
+         while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            // Traverse Right
+            for (int j = colBegin; j <= colEnd; j ++) {
+                res.add(matrix[rowBegin][j]);
+            }
+            rowBegin++;
+
+            // Traverse Down
+            for (int j = rowBegin; j <= rowEnd; j ++) {
+                res.add(matrix[j][colEnd]);
+            }
+            colEnd--;
+
+            if (rowBegin <= rowEnd) { // check avoids double counting in single row case
+                // Traverse Left
+                for (int j = colEnd; j >= colBegin; j --) {
+                    res.add(matrix[rowEnd][j]);
+                }
+            }
+            rowEnd--;
+
+            if (colBegin <= colEnd) {
+                // Traver Up
+                for (int j = rowEnd; j >= rowBegin; j --) {
+                    res.add(matrix[j][colBegin]);
+                }
+            }
+            colBegin ++;
+        }
+
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        SpiralMatrix obj = new SpiralMatrix();
+//        int[][] matrix = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        };
+//        int[][] matrix = {
+//                {1, 5, 9},
+//                {2, 6, 9},
+//                {3, 7, 11},
+//                {4, 8, 2}
+//        };
+//        int[][] matrix = {
+//                {1, 5},
+//                {2, 6},
+//        };
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+        };
+        System.out.println(obj.spiralOrder(matrix));
+    }
+}
